3739dcf676d82d890b00a33e56d28c18fd325eb3
victoricoma <victor.icoma@etec.sp.gov.br>
2024-04-01 11:58:10 -0300

    Correções e adições importantes

---
+++ StockApp/StockApp.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StockApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(t => t.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
---
+++ StockApp/StockApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using StockApp.Domain.Entities;
+using StockApp.Domain.Interfaces;
+using StockApp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+    }
+}
